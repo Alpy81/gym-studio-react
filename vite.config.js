5db007500feca6eab1a93f65736762b8b6f1f654
Alpy81 <alperen.ertas@gmx.de>
2024-02-08 14:36:14 +0100

    update commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: "/github.com/Alpy81",
+  base: "/github.com/Alpy81/",
   ...(command !== "serve" && { base: "/gym-studio-react/" }),
-});
+}));
